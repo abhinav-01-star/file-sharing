02591acde0f5dc742de14b4d814980b886b5fa63
unknown <simhadriabhinav111@emai.com>
2022-10-13 19:59:28 +0530

    c-1

--- routes/files.js
+++ routes/files.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const multer = require('multer');
 const path = require('path');
 const File = require('../models/file');
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuid4 } = require('uuid');
 
 let storage = multer.diskStorage({
     destination: (req, file, cb) => cb(null, 'uploads/') ,
@@ -21,7 +21,7 @@ router.post('/', (req, res) => {
       }
         const file = new File({
             filename: req.file.filename,
-            uuid: uuidv4(),
+            uuid: uuid4(),
             path: req.file.path,
             size: req.file.size
         });
@@ -49,7 +49,7 @@ router.post('/send', async (req, res) => {
     sendMail({
       from: emailFrom,
       to: emailTo,
-      subject: 'inShare file sharing',
+      subject: 'INSHARE FILESHARING',
       text: `${emailFrom} shared a file with you.`,
       html: require('../services/emailTemplate')({
                 emailFrom, 
